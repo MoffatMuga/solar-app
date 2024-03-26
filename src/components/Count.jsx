21b39006ba6cd1bf2306b1dfd6a7bc114d77ed8e
Moffat <moffat.muga@gmail.com>
2024-03-26 21:57:55 +0300

    add Count content

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -16,28 +16,28 @@ export default function Count() {
                 data-aos-duration="2000" className='flex rounded shadow-xl flex-col border-2 border-lg  items-center h-35 w-25 px-2 bg-white'>
             <IoIosPeople className='text-6xl text-main-1' />
             <h2 className='text-xl text-black/70 font-bold'><CountUp start={0} end={40} duration={1} suffix='+'  /></h2>
-            <h4 className='text-lg text-center text-black/70'>projects <br className='md:hidden'/>completed </h4>
+            <h4 className='text-lg text-center font-bold text-black/70'>projects <br className='md:hidden'/>completed </h4>
         </div>
         <div data-aos="flip-left"
                 data-aos-easing="ease-out-cubic"
                 data-aos-duration="2000" className='flex flex-col rounded shadow-lg border-2 border-lg  items-center h-35 w-25 px-2 bg-white'>
             <FaPeopleCarry className='text-6xl text-main-1'/>
-            <h2 className='text-xl  text-black/70 font-bold'><CountUp start={0} end={40} duration={1} suffix='+' /></h2>
-            <h4 className='text-lg text-center text-black/70'>projects <br className='md:hidden'/>completed </h4>
+            <h2 className='text-xl  text-black/70 font-bold'><CountUp start={0} end={100} duration={1} suffix='%' /></h2>
+            <h4 className='text-lg text-center font-bold text-black/70'>client <br className='md:hidden'/>handovers </h4>
         </div>
         <div data-aos="flip-left"
                 data-aos-easing="ease-out-cubic"
                 data-aos-duration="2000" className='flex flex-col rounded shadow-lg border-2 border-lg  items-center h-35 w-25 px-2 bg-white'>
             <BiHappyHeartEyes className='text-6xl text-main-1'/>
-            <h2 className='text-xl  text-black/70 font-bold'><CountUp start={0} end={40} duration={1} suffix='+'  /></h2>
-            <h4 className='text-lg text-center text-black/70'>projects <br className='md:hidden'/>completed </h4>
+            <h2 className='text-xl  text-black/70 font-bold'><CountUp start={0} end={96} duration={1} suffix='%'  /></h2>
+            <h4 className='text-lg text-center font-bold text-black/70'>satisfied <br className='md:hidden'/>clients </h4>
         </div>
         <div data-aos="flip-left"
                 data-aos-easing="ease-out-cubic"
                 data-aos-duration="2000" className='flex flex-col rounded shadow-lg border-2 border-lg  items-center h-35 w-25 px-2 bg-white'>
             <MdEngineering className='text-6xl text-main-1'/>
-            <h2 className='text-xl  text-black/70 font-bold'><CountUp start={0} end={40} duration={1} suffix='+' /></h2>
-            <h4 className='text-lg text-center text-black/70'>projects <br className='md:hidden'/>completed </h4>
+            <h2 className='text-xl  text-black/70 font-bold'><CountUp start={0} end={10} duration={1} suffix='+' /></h2>
+            <h4 className='text-lg text-center font-bold text-black/70'>qualified <br className='md:hidden'/>personell </h4>
         </div>
         
     </div>
